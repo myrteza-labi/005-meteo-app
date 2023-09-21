06f014193ac9bb6b5d694292fd310b5464a46363
myrteza <myrteza@hotmail.fr>
2023-09-21 18:23:50 +0200

    project done autonome

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require('express'); 
+const cors = require('cors'); 
+const axios = require('axios'); 
 
 const app = express(); 
-const port = 3001; 
-const apiKey = '50341f938464fac53604f2dbc82940dc'; 
-
 app.use(cors()); 
+const port = 3001; 
+const apiKey = '50341f938464fac53604f2dbc82940dc';
 
 app.get('/weather', async (req, res) => {
   try {
     const city = req.query.city; 
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`); 
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
     const weatherData = {
       temperature : response.data.main.temp, 
       description: response.data.weather[0].description,
@@ -21,11 +20,11 @@ app.get('/weather', async (req, res) => {
   catch (error) {
     console.error(error); 
     res.status(500).json({
-      error: "Erreur lors de la récupération des données météo"
+      error: "Erreur lors de l importation des données météo"
     })
   }
 })
 
 app.listen(port, () => {
-  console.log(`Serveur en cours d éxécution sur le port ${port}`)
+  console.log(`Serveur en cours d'éxécution sur le port ${port}`)
 })
